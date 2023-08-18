65f2e0bde5d08f921c6008655c7812e5b498e64f
AleksRodkin <aleksrodkin95@gmail.com>
2023-08-18 14:20:23 +0700

    Fixed PrintArray func + corrected README.md

--- Work files/Program.cs
+++ Work files/Program.cs
@@ -30,13 +30,15 @@ string[] CreateNewArray(int length)
 
 void PrintArray(string[] array)
 {
+  Console.Write("[");
   for (int i = 0; i < array.Length; i++)
   {
-    if (i == 0) Console.Write($"['{array[i]}', ");
+    if (i == 0) Console.Write($"'{array[i]}', ");
     else if (i < array.Length - 1)
       Console.Write($"'{array[i]}', ");
-    else Console.Write($"'{array[i]}']");
+    else Console.Write($"'{array[i]}'");
   }
+  Console.Write("]");
   Console.WriteLine("");
 }
 
--- Work files/README.md
+++ Work files/README.md
@@ -5,11 +5,9 @@
 ## Algorithm:
 1. Request the length of the array;
 2. Request the strings;
-3. Fill in the array;
+3. Create and fill in the array;
 4. Show array;
 5. Look for strings that are less than or equal to 3 characters;
-6. Create a counter for selected strings;
-7. Create a new array;
-8. Fill in the new array;
-9. Show the new array.
+6. Create and fill in a new array with new strings;
+7. Show the new array.
 
